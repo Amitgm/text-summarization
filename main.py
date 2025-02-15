242fc2ae4db0a0b6a33841f96c0ee95ad4299e51
amit george <amittimer@gmail.com>
2025-02-15 13:10:38 +0400

    data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textsummarizer.logging import logger
 
 from src.textsummarizer.pipeline.stage_data_ingestion_pipeline import DataIngestionpipeline
+from src.textsummarizer.pipeline.stage_data_processing_pipeline import DataTransformationpipeline
 
 logger.info("logging is implemented")
 
@@ -14,6 +15,24 @@ try:
     data_ingestion_pipeline = DataIngestionpipeline()
 
     data_ingestion_pipeline.initiate_data_ingestion()
+
+    logger.info("stage {STAGE_NAME} completed")
+
+except Exception as e:
+
+    logger.exception(e)
+
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+
+    logger.info("stage {STAGE_NAME} initiated")
+
+    data_processing_pipeline = DataTransformationpipeline()
+
+    data_processing_pipeline.initiate_data_transformation()
 
     logger.info("stage {STAGE_NAME} completed")
 
---
+++ src/textsummarizer/pipeline/stage_data_processing_pipeline.py
@@ -0,0 +1,20 @@
+
+from src.textsummarizer.config.configuration import ConfigurationManager
+from src.textsummarizer.components.data_transformation import DataTransformation
+from src.textsummarizer.logging import logger
+
+
+class DataTransformationpipeline:
+
+    def __init__(self):
+
+        pass
+    def initiate_data_transformation(self):
+
+        config = ConfigurationManager()
+
+        data_transformation_config = config.get_data_transformation_config()
+
+        data_transformation = DataTransformation(config=data_transformation_config)
+
+        data_transformation.convert()
